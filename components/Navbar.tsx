fa5d4148e99d6d2d3eedcd620e9c770da51b8840
2ono <clt3555@gmail.com>
2023-12-29 12:43:28 +0900

    [add] sign in google, github

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,37 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useSession, signOut } from "next-auth/react";
 
 const Navbar = () => {
+  const { status } = useSession();
   return (
     <div className="flex justify-between pb-4 border-b mb-4">
       <div>
         <Link href={"/"}>
-          <h1 className="text-dark text-4xl font-bold tracking-tighter">Tech News</h1>
+          <h1 className="text-dark text-4xl font-bold tracking-tighter">
+            Tech News
+          </h1>
           <p className="text-sm">
             Expoloring Tomorriw's Innovations, <br />
             One Bytes at a Time
           </p>
         </Link>
       </div>
-      <div className="flex items-center">
-        <Link className="btn" href={"/sign-in"}>Sign In</Link>
-      </div>
+      {status === "authenticated" ? (
+        <div>
+          <button className="btn" onClick={() => signOut()}>
+            Sign out
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center">
+          <Link className="btn" href={"/sign-in"}>
+            Sign In
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
--- components/SignInBtns.tsx
+++ components/SignInBtns.tsx
@@ -1,12 +1,15 @@
+"use client"
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
+
 
 const SignInBtns = () => {
   return (
     <>
       <h1 className="text-center mt-8">Sign In</h1>
       <div className="mt-4 p-4 flex flex-col items-center justify-center gap-4">
-        <button
+        <button onClick={() => signIn('github')}
           className="flex items-center border p-5 rounded-full gap-4
         hover:bg-slate-100/25 transition"
         >
@@ -22,6 +25,7 @@ const SignInBtns = () => {
         </button>
 
         <button
+        onClick={() => signIn('google')}
           className="flex items-center border p-5 rounded-full gap-4
         hover:bg-slate-100/25 transition"
         >
